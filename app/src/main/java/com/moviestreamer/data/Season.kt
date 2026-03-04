5de7f1be07054ad52f45a07f74c1adfce7f483d1
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2026-03-04 14:26:04 +0300

    Model changes

--- app/src/main/java/com/moviestreamer/data/Season.kt
+++ app/src/main/java/com/moviestreamer/data/Season.kt
@@ -28,33 +28,3 @@ data class Season(
         return posterPath?.let { "https://image.tmdb.org/t/p/w500$it" }
     }
 }
-
-data class Episode(
-    @SerializedName("id")
-    val id: Int,
-
-    @SerializedName("episode_number")
-    val episodeNumber: Int,
-
-    @SerializedName("season_number")
-    val seasonNumber: Int,
-
-    @SerializedName("name")
-    val name: String?,
-
-    @SerializedName("overview")
-    val overview: String?,
-
-    @SerializedName("air_date")
-    val airDate: String?,
-
-    @SerializedName("vote_average")
-    val voteAverage: Double?,
-
-    @SerializedName("still_path")
-    val stillPath: String?
-) {
-    fun getStillUrl(): String? {
-        return stillPath?.let { "https://image.tmdb.org/t/p/w500$it" }
-    }
-}
